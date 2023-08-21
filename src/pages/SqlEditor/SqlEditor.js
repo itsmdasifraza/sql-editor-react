779c98d599871a68ddcb8208e81d13fc476f6e71
Md Asif Raza <itsmdasifraza@gmail.com>
2023-08-22 03:25:17 +0530

    Add table list to the sidebar (#5)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/pages/SqlEditor/SqlEditor.js
+++ src/pages/SqlEditor/SqlEditor.js
@@ -1,9 +1,20 @@
 import './SqlEditor.css';
 import React from 'react'
+import { Sidebar } from '../../components/Sidebar/Sidebar';
+import Grid2 from '@mui/material/Unstable_Grid2';
 
 function SqlEditor() {
   return (
-    <div>SqlEditor</div>
+    <div className='sql-editor'>
+        <Grid2 container spacing={0} >
+            <Grid2 xs={12} sm={3} md={3} >
+                <Sidebar/>
+            </Grid2>
+            <Grid2 xs={12} sm={1} md={3} >
+                
+            </Grid2>
+        </Grid2>
+    </div>
   )
 }
 
